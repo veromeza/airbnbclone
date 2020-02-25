176400bd3b6f9413a173f73b1e4f4217b6c69484
veronica <lucia.meza81@gmail.com>
2020-02-24 21:51:24 -0600

    verificacion del token

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const { ApolloServer, gql } = require('apollo-server');
 const {importSchema} = require('graphql-import');
 const mongoose = require('mongoose');
 const resolvers = require('./resolvers');
+const AuthDirective = require ('./resolvers/Directives/AuthDirective');
+const verifyToken = require('./utils/verifyToken');
 
 
 async function start () {
@@ -23,7 +25,14 @@ async function start () {
     mongo.on ('error', error => console.log(error))
          .once('open',() => console.log('Connected to database'))
     
-    const server = new ApolloServer({typeDefs,resolvers})
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        schemaDirectives:{
+            auth:AuthDirective
+        },
+        context: ({req}) => verifyToken(req)
+    });
     
     server.listen().then(({url}) => {
         console.log(`Server ready set: ${url}`)
---
+++ server/utils/verifyToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const Users = require('../models/users');
+
+module.exports = async (request) => {
+
+    const Authorization = request.get('Authorization');
+    if(Authorization){
+        const formatedToken = Authorization.replace('JWT ','');
+        const payload =jwt.verify(formatedToken,process.env.SECRET);
+        if(!payload) throw new Error ('Token is invalid');
+        const user = await Users.findOne({email:payload.email});
+        if(!user) throw new Error ('User does not exist');
+        return {...request, user};
+    }else{
+        return request;
+    }
+    };
